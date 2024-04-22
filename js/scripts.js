53964e2514635756b823d289b9cebdbaa8e76e76
defrank1 <134450565+defrank1@users.noreply.github.com>
2024-04-22 16:36:50 -0400

    troubleshooting

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,6 @@ var mapOptions = {
     zoom: 10.4, // starting zoom,
 }
 
-// instantiate the map
-const map = new mapboxgl.Map(mapOptions);
 
 // add a navitation control
 const nav = new mapboxgl.NavigationControl();
