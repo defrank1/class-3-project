c51f2b5dbca72e89956f0c5ca10576e5f94b7c47
defrank1 <134450565+defrank1@users.noreply.github.com>
2024-04-24 15:12:15 -0400

    trying to create a line using a new file to contain the line coordinates

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGVmcmFuazEiLCJhIjoiY2x1bHZ0OWJyMHlhdjJrcDFsZzlwc3ZxMSJ9.XD1OM3LMVn2qoX9QMqR5Vg';
 var mapOptions = {
     container: 'map', // container ID
@@ -8,37 +6,37 @@ var mapOptions = {
     zoom: 10.4, // starting zoom,
 }
 
+// map.js
+const mapboxgl = require('mapbox-gl'); // Assuming you use Node.js-style module importing
+
+// Import the redlineline coordinates from your separate file
+const redlineline = require('js/line-data.js'); // Update the path as needed
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGVmcmFuazEiLCJhIjoiY2x1bHZ0OWJyMHlhdjJrcDFsZzlwc3ZxMSJ9.XD1OM3LMVn2qoX9QMqR5Vg';
+
 // instantiate the map
 const map = new mapboxgl.Map(mapOptions);
 
-// a rough polygon of NY state border, hand-drawn in geojson.io
+// a rough line of the WMATA Red Line in Ward 3 
 var redlineline = {
     "type": "FeatureCollection",
     "features": [
-      {
-        "type": "Feature",
-        "properties": {},
-        "geometry": {
-          "coordinates": [
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates":
                     [
+
                         [
-                            -77.05142159827537,
-                            38.923537689351264
-                          ],
-                          [
-                            -77.06499044288512,
-                            38.94674138797188
-                          ],
-                          [
-                            -77.07910946840825,
-                            38.94677559605742
-                          ],
-                          [
-                            -77.08667715248745,
-                            38.96268421761414
-                          ]
-                    ]
-                ],
+                            -76.960274,
+                            38.897077
+                        ],
+
+
+
+
+                    ],
                 "type": "LineString"
             }
         }
@@ -57,6 +55,7 @@ map.on('load', () => {
         "type": "geojson",
         "data": redlineline
     })
+
     // add a line layer using this dummy data
     map.addLayer({
         'id': 'redlineline-line',
@@ -65,35 +64,54 @@ map.on('load', () => {
         'layout': {},
         'paint': {
             'line-color': 'red',
-            'line-width': 15,
-            'line-dasharray': [2, 2]
+            'line-width': 10,
         }
-    }, );
+    },);
 
     // list all the layers on the map in the console
     console.log(
         map.getStyle().layers
     )
 
-// loop over the stationData array to make a marker for each record
-stationData.forEach(function (stationRecord) {
+    // loop over the stationData array to make a marker for each record
+    stationData.forEach(function (stationRecord) {
 
-    // create a popup to attach to the marker
-    const popup = new mapboxgl.Popup({
-        offset: 24,
-        anchor: 'bottom'
-    }).setText(
-        `${stationRecord.station_name} is a WMATA metro station on the ${stationRecord.station_line} Line`
-    );
-    const el = document.createElement('div');
-    el.className = 'single-station';
-    if (stationRecord.transfer_station === true) {
-        el.className = 'transfer-station'
+        // create a popup to attach to the marker
+        const popup = new mapboxgl.Popup({
+            offset: 24,
+            anchor: 'bottom'
+        }).setText(
+            `${stationRecord.station_name} is a WMATA metro station on the ${stationRecord.station_line} Line`
+        );
+        const el = document.createElement('div');
+        el.className = 'single-station';
+        if (stationRecord.transfer_station === true) {
+            el.className = 'transfer-station'
+        }
+
+        // create a marker, set the coordinates, add the popup, add it to the map
+        new mapboxgl.Marker(el)
+            .setLngLat([stationRecord.longitude, stationRecord.latitude])
+            .setPopup(popup)
+            .addTo(map);
+    })
+
+    // loop over the lineData array to make a rail line for each record
+    lineData.forEach(function (lineRecord) {
+
+        // create a line, set the coordinates, add it to the map
+        new mapboxgl.addLayer({
+        id: 'bluea1line',
+        type: 'lineString',
+        source: 'js/station-data/js',
+        layout: {
+'line-cap': 'round',
+'line-join': 'round'
+
+        },
+        paint: {
+            'line-color': '#888',
+        'line-width': 5
     }
-
-    // create a marker, set the coordinates, add the popup, add it to the map
-    new mapboxgl.Marker(el)
-        .setLngLat([stationRecord.longitude, stationRecord.latitude])
-        .setPopup(popup)
-        .addTo(map);
-})});
+    })
+})})
